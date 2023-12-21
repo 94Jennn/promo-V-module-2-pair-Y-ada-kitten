0aae6856e167f61d5f319d61f7857b71abb5e521
queralt-chan <queraltbali@gmail.com>
2023-12-21 17:52:07 +0800

    conflicts

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const kittenOne = `<li class="card">
     src="https://dev.adalab.es/gato-siames.webp"
     alt="gatito"
   />
-  <h3 class="card_title">Anastacio</h3>
+  <h3 class="card_title"> Anastacio </h3>
   <h4 class="card_race">Siamés</h4>
   <p class="card_description">
             Porte elegante, su patrón de color tan característico y sus ojos
@@ -19,14 +19,12 @@ const kittenOne = `<li class="card">
             hace 500 años, donde tuvo su origen muy posiblemente.
    </p>
 </article>
-</li>`;
+</li>`
 
-const kittenTwo = '<li class="card">
-<img
-  class="card_img"
-  src="https://dev.adalab.es/sphynx-gato.webp"
-  alt="sphynx-cat"
-/>
+const kittenTwo = `<li class="card">
+  <img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp"
+  alt="sphynx-cat" />
+
 <h3 class="card_title">Fiona</h3>
 <h4 class="card_race">Sphynx</h4>
 <p class="card_description">
@@ -34,8 +32,29 @@ const kittenTwo = '<li class="card">
   hasta con pinta de alienígena han llegado a definir a esta raza
   gatuna que se caracteriza por la «ausencia» de pelo.
 </p>
-</li>'
+</li>`
+
+const kittenThree = `<li class="card">
+<img
+  class="card_img"
+  src="https://dev.adalab.es/maine-coon-cat.webp"
+  alt="maine-coon-cat"
+/>
+<h3 class="card_title">Cielo</h3>
+<h4 class="card_race">Maine Coon</h4>
+<p class="card_description">
+  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+  bella mirada se ha convertido en una de sus señas de identidad.
+  Sus ojos son grandes y las orejas resultan largas y en punta.
+</p>
+</li>`
 
 
+list.innerHTML = kittenOne + kittenTwo + kittenThree 
 
-list.innerHTML = kittenOne + kittenTwo + 
+const kittenImage = `https://dev.adalab.es/gato-siames.webp`;
+const kittenName = `Anastacio`;
+const kittenDesc = `Porte elegante, su patrón de color tan característico y sus ojos
+de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente.`;
+const kittenRace = `Siamés`;
